a7f71eecac93534c26c776bd7a9b48ef978d7af8
Topbit Du <topbit.du@gmail.com>
2017-05-08 21:57:54 +0800

    1, Improve the As Certification shared examples for the argument validations of the #certificate! method, & the #certificate? method.

--- lib/rspec/models/unidom/certificate/concerns/as_certification_shared_examples.rb
+++ lib/rspec/models/unidom/certificate/concerns/as_certification_shared_examples.rb
@@ -1,4 +1,12 @@
-shared_examples 'Unidom::Certificate::Concerns::AsCertification' do |model_attributes|
+shared_examples 'Unidom::Certificate::Concerns::AsCertification' do |model_attributes, model, certificated, certificator|
+
+  #subject do described_class.create! model_attributes end
+  #it '#certificate!' do
+    it_behaves_like 'assert_present!', model, :certificate!, [ certificated, { by: certificator, at: Time.now } ], [ { 0 => :certificated }, :by, :at ]
+  #end
+  #it '#certificate?' do
+    it_behaves_like 'assert_present!', model, :certificate?, [ certificated, {                   at: Time.now } ], [ { 0 => :certificated },      :at ]
+  #end
 
   context do
 
@@ -17,6 +25,14 @@ shared_examples 'Unidom::Certificate::Concerns::AsCertification' do |model_attri
       }
 
     it_behaves_like 'has_many', model_attributes, :certificatings, Unidom::Certificate::Certificating, [ certificating_1_attribtues, certificating_2_attribtues ]
+
+  end
+
+  context do
+
+    #model = described_class.create! model_attributes
+    #it_behaves_like 'assert_present!', model, :certificate!, [ certificated, { by: certificator, at: Time.now } ], [ { 0 => :certificated }, :by, :at ]
+    #it_behaves_like 'assert_present!', model, :certificate?, [ certificated, {                   at: Time.now } ], [ { 0 => :certificated },      :at ]
 
   end
 
